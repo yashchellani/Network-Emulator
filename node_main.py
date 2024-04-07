8fcc52c58a2976883f80b68570f1c853ffaaf8cb
Yash Chellani <35882565+yashchellani@users.noreply.github.com>
2024-04-08 03:49:44 +0800

    ensure consistency in running nodes

--- node_main.py
+++ node_main.py
@@ -10,6 +10,24 @@ def load_firewall_rules(filepath="config/rules.json"):
     data = json.load(file)
     return data["rules"]
 
+def record_node_running(node_type, running):
+   "Helper method to ensure that one node type can only be run once"
+   existing_nodes = ""
+   with open("active_nodes.txt", "r+") as file:
+      if running:
+         if node_type in existing_nodes:
+            raise RuntimeError("Node is already running")
+         existing_nodes += node_type + "\n"
+      else:
+         existing_nodes = existing_nodes.replace(node_type + "\n", "")
+      file.seek(0)
+      file.write(existing_nodes) 
+      file.truncate()
+def clean_running_nodes_file():
+   with open("active_nodes.txt", "w") as file:
+      file.write("")
+
+
 firewall_rules = load_firewall_rules()
 firewall = Firewall(rules=firewall_rules)
 ids = IDS(firewall=firewall)
@@ -22,7 +40,18 @@ node_configurations = {
 }
 
 if __name__ == '__main__':
-    node_type = input("Which type of node do you want to create? (node1, node2, node3, node4) :")
+    clean_running_nodes_file()
+    while True:
+      node_type = input("Which type of node do you want to create? (node1, node2, node3, node4) :")
+      if node_type not in node_configurations.keys():
+         print("Invalid node type")
+      else:
+         try:
+            record_node_running(node_type, True)
+            break
+         except Exception as e:
+            print(e)
+         
     node = node_configurations[node_type]
     node.connect_to_data_link()
     node.start_receiving()
@@ -50,6 +79,7 @@ if __name__ == '__main__':
            node.send_ip_packet("KILL", dest_node.ip_address, protocol=1)
 
            sleep(10)
+
         elif command == "ip_spoof":
            if not isinstance(node, MaliciousNode):
               print("You are a benign node, unable to spoof!")
@@ -75,6 +105,7 @@ if __name__ == '__main__':
               node.arp_spoof(dest_node.ip_address, fake_ip)
 
         elif command == "exit":
+           record_node_running(node_type, False)
            node.stop_receiving()
            print("Goodbye!")
            exit()
